6ada002c8a0d5c6361b3e4edb9cdc2da22680d9b
igor <i.leturia@elhuyar.eus>
2017-04-24 10:53:25 +0200

    Fix #7: Scroll issue in Chrome and Edge solved thanks to contribution by @rmcrowley2000

--- plugin/toc-progress/toc-progress.js
+++ plugin/toc-progress/toc-progress.js
@@ -333,7 +333,7 @@ toc_progress.reduceorscrollelementifnecessary=function(element)
 			{
 				visible_element_index=visible_element_index+1;
 			};
-			if (window.getComputedStyle(li_element).getPropertyValue('font-weight')=='700')
+			if (window.getComputedStyle(li_element).getPropertyValue('font-weight')=='700' || window.getComputedStyle(li_element).getPropertyValue('font-weight')==700 || window.getComputedStyle(li_element).getPropertyValue('font-weight')=='bold')
 			{
 				selected_element_index=visible_element_index;
 			};
